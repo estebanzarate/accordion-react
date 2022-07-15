938e54afb38d7c891bb2caa1df7d07e3e842883a
estebanzarate <esteban-zarate@hotmail.com>
2022-07-15 14:23:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './SingleQuestion';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [questions, setQuestions] = useState(data)
+
+	return (
+		<main className='container'>
+			<h3>Preguntas y respuestas</h3>
+			<section className='info'>
+				{questions.map(question => <SingleQuestion key={question.id} {...question} />)}
+			</section>
+		</main>
+	);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data = [
+	{
+		id: 1,
+		title: 'Primer pregunta',
+		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et rem distinctio in nihil adipisci laboriosam autem sapiente impedit pariatur magni dolor cum accusamus odit ipsam cupiditate, fuga laudantium repellendus praesentium quo dolorem molestias at? Nemo, at fuga, labore quaerat assumenda exercitationem eum nesciunt minus harum, eveniet similique cupiditate neque officiis?'
+	},
+	{
+		id: 2,
+		title: 'Segunda pregunta',
+		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et rem distinctio in nihil adipisci laboriosam autem sapiente impedit pariatur magni dolor cum accusamus odit ipsam cupiditate, fuga laudantium repellendus praesentium quo dolorem molestias at? Nemo, at fuga, labore quaerat assumenda exercitationem eum nesciunt minus harum, eveniet similique cupiditate neque officiis?'
+	},
+	{
+		id: 3,
+		title: 'Tercer pregunta',
+		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et rem distinctio in nihil adipisci laboriosam autem sapiente impedit pariatur magni dolor cum accusamus odit ipsam cupiditate, fuga laudantium repellendus praesentium quo dolorem molestias at? Nemo, at fuga, labore quaerat assumenda exercitationem eum nesciunt minus harum, eveniet similique cupiditate neque officiis?'
+	},
+	{
+		id: 4,
+		title: 'Cuarta pregunta',
+		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et rem distinctio in nihil adipisci laboriosam autem sapiente impedit pariatur magni dolor cum accusamus odit ipsam cupiditate, fuga laudantium repellendus praesentium quo dolorem molestias at? Nemo, at fuga, labore quaerat assumenda exercitationem eum nesciunt minus harum, eveniet similique cupiditate neque officiis?'
+	},
+	{
+		id: 5,
+		title: 'Quinta pregunta',
+		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et rem distinctio in nihil adipisci laboriosam autem sapiente impedit pariatur magni dolor cum accusamus odit ipsam cupiditate, fuga laudantium repellendus praesentium quo dolorem molestias at? Nemo, at fuga, labore quaerat assumenda exercitationem eum nesciunt minus harum, eveniet similique cupiditate neque officiis?'
+	}
+]
+
+export default data;
